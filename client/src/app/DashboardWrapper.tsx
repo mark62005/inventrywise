157fb88b0da9f3902feb9d5021cf0b29bd7cd03b
Mark Wong <markwong.digital@gmail.com>
2025-04-15 16:53:00 -0700

    Build and implement Sidebar component

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -1,12 +1,25 @@
+"use client";
+
 import { LayoutProps } from "@/types/app/page-props";
+import Sidebar from "@/components/shared/Sidebar";
 import Navbar from "@/components/shared/Navbar";
+import { useAppSelector } from "@/state/redux";
+import { selectIsSidebarCollapsed } from "@/state/slices/globalSlice";
+import { cn } from "@/lib/utils";
 
 function DashboardWrapper({ children }: LayoutProps) {
+	const isSidebarCollapsed = useAppSelector(selectIsSidebarCollapsed);
+
 	return (
 		<div className="flex w-full min-h-screen bg-gray-50 text-gray-900">
-			{/* TODO: Create Sidebar component */}
-			Sidebar
-			<main className="flex flex-col w-full h-full py-5 px-9 bg-gray-50 md:pl-24">
+			<Sidebar />
+
+			<main
+				className={cn(
+					"flex flex-col w-full h-full py-5 px-9 bg-gray-50",
+					isSidebarCollapsed ? "md:pl-24" : "md:pl-72"
+				)}
+			>
 				<Navbar />
 
 				{children}
