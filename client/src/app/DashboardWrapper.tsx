6b96d430676678673b89929f8bd9831687466a26
Mark Wong <markwong.digital@gmail.com>
2025-04-15 15:15:34 -0700

    Build and implement Navbar (in progress)

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -1,11 +1,14 @@
 import { LayoutProps } from "@/types/app/page-props";
+import Navbar from "@/components/shared/Navbar";
 
 function DashboardWrapper({ children }: LayoutProps) {
 	return (
 		<div className="flex w-full min-h-screen bg-gray-50 text-gray-900">
 			{/* TODO: Create Sidebar component */}
 			Sidebar
-			<main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24">
+			<main className="flex flex-col w-full h-full py-5 px-9 bg-gray-50 md:pl-24">
+				<Navbar />
+
 				{children}
 			</main>
 		</div>
