e39b6b8cc9824dcaeb17e6adb8c2c97a35b3a2bd
Mark Wong <markwong.digital@gmail.com>
2025-04-15 14:50:09 -0700

    Build DashboardWrapper component (in progress)

--- client/src/app/DashboardWrapper.tsx
+++ client/src/app/DashboardWrapper.tsx
@@ -2,10 +2,12 @@ import { LayoutProps } from "@/types/app/page-props";
 
 function DashboardWrapper({ children }: LayoutProps) {
 	return (
-		<div className="">
+		<div className="flex w-full min-h-screen bg-gray-50 text-gray-900">
 			{/* TODO: Create Sidebar component */}
 			Sidebar
-			<main className="">{children}</main>
+			<main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24">
+				{children}
+			</main>
 		</div>
 	);
 }
